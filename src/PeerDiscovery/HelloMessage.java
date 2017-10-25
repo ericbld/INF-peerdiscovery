25a07c0979e6d38143d6cf95d8194933878644b7
AmaurX <amaury.camus92@gmail.com>
2017-10-25 19:11:55 +0200

    Added first hello constructor (#4)
    * Added first hello constructor
    
    * added second constructor
    
    * added getHelloMessageAsEncodedString

--- src/PeerDiscovery/HelloMessage.java
+++ src/PeerDiscovery/HelloMessage.java
@@ -6,15 +6,95 @@
 package PeerDiscovery;
 
 import java.util.List;
+import java.util.Vector;
 
 /**
  *
  * @author arpaf
  */
 public class HelloMessage {
-	int senderID;
+	String senderID;
 	int sequenceNumber;
-	int HelloInterval;
-	int NumPeers;
-	List<Integer> peers;
+	int helloInterval;
+	int numPeers;
+	Vector<String> peers;
+
+	static public String HELLO = "HELLO"; 
+
+
+	/*
+	*	Constructor from a fromatted string
+	*	Initializarion with given peers
+	*/
+	public HelloMessage(String s) throws Exception
+	{
+		String slist[] = s.split(";");
+
+		if(slist.length < 5){
+			throw new Exception("Missing arguments in the hello string");
+		}
+
+		if(!slist[0].equals(HELLO))
+		{
+			throw new Exception("Not a Hello message");
+		}
+
+		senderID = slist[1];
+		sequenceNumber = Integer.parseInt(slist[2]);
+		helloInterval = Integer.parseInt(slist[3]);
+		numPeers = Integer.parseInt(slist[4]);
+
+		if(numPeers != (slist.length - 5))
+		{
+			throw new Exception("Wrong number of peer given...");
+		}
+
+		peers = new Vector<String>();
+
+		for(int i = 5; i < (5 + numPeers); i++)
+		{
+			peers.add(slist[i]);
+		}
+	}
+
+
+	/*
+	*	Constructor from info
+	*	Initializarion with no peers
+	*/
+	public HelloMessage(String senderIdIn, int sequenceNo, int helloIntervalIn)
+	{
+		senderId = senderIdIn;
+		senquenceNumber = sequenceNo;
+		helloInterval = helloIntervalIn;
+		numPeers = 0;
+		peers = new Vector<String>();		
+	}
+
+
+	public String getHelloMessageAsEncodedString() throws Exception
+	{
+		String result = HELLO;
+		result += ";";
+		result += senderID;
+		result += ";";
+		result += senquenceNumber;
+		result += ";";
+		result += helloInterval;
+		result += ";";
+		result += numPeers;
+
+		if(numPeers != peers.size())
+		{
+			throw new Exception("numPeer isn't equal to peers.size().");
+		}
+
+		for(int i = 0; i < numPeers; i++)
+		{
+			result += ";";
+			result += peers.elementAt(i);
+		}
+
+		return result;
+	}
 }
